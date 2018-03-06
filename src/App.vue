4ffc0088f8bd96432923b84f63cfec95c6d59cb4
Ewen Billot <ewen.billot@Digital-Grenoble.local>
2018-03-06 11:37:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIxkKLbeYYp6IhrA2sRnUr6gxI0IP67M9YpvWzZI-0QEl41C7yhw">
+    <div>
+      <button id="listMachine" @click="greet">Listes des Machines</button> <button id="showMap" @click="">Voir la carte</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome on overwatch coffe machine',
+
+      methods:{
+        el: '#listMachine',
+        greet: function(event){
+          console.log('click')
+        }
+      }
+    }
+  },
+  methods: {
+    greet: function (event) {
+      console.log('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+button {
+  background-color: aqua;
+  border: 2px solid black;
+  font-weight: normal;
+}
+</style>
